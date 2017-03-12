06001da52b6280fe73c006a469a4a27362e01314
Cam Crews <camcrews@gmail.com>
2017-03-12 18:12:49 -0400

    tweak

--- server.rb
+++ server.rb
@@ -53,100 +53,39 @@ end
 
 get '/auth/slack/callback' do
   p request
-  request.env["omniauth.params"]
+  ENV["RACK_ENV"]["omniauth.params"]
   #@user = User.find_by(slack_name: ENV['omniauth.auth']['user']['name'])
-
- # ENV['omniauth.auth']
-  # # env['omniauth.auth']
-  #  p "detected provider #{env['omniauth.auth']['provider']}"
-  #   oa_account = DB[:oa_account].where(id: env['omniauth.auth']['uid']).where(type: env['omniauth.auth']['provider'])
-  #   p oa_account.to_a
-  #   if 1 != oa_account.update(token: env['omniauth.auth']['credentials']['token']) && oa_account.to_a.length < 1
-
-  #     # try to find a user account linked to this oa_account...  if not found, this oa_account
-  #     # will be treated as a provisional application for a user account.  An administrator 
-  #     # can then link an oa_account to a user account
-  #     names = env['omniauth.auth']['extra']['raw_info']['name'].split(" ")
-  #     n_m = nil
-  #     n_m = names[1..-2].join(' ') if names && names.length >= 2
-  #     img = open(env['omniauth.auth']['info']['image'], :allow_redirections => :safe).base_uri.to_s
-  #     IO.copy_stream(open(img), "public/img/#{env['omniauth.auth']['provider']}/#{env['omniauth.auth']['uid']}.jpg")
-
-  #     p env['omniauth.auth']
-
-  #     #let's create a user account for this person...
-  #     n_f = env['omniauth.auth']['info']['first_name']
-  #     n_f = env['omniauth.auth']['extra']['raw_info']['first_name'] if n_f.nil? || n_f.length < 0
-  #     n_l = env['omniauth.auth']['info']['last_name']
-  #     n_l = env['omniauth.auth']['extra']['raw_info']['last_name'] if n_l.nil? || n_l.length < 0
-
-  #     user = DB[:user].where(
-  #       email: env['omniauth.auth']['info']['email']
-  #     ).first
-  #     if( user.nil? ) 
-  #       user_id = DB[:user].insert(
-  #         n_f: n_f,
-  #         n_l: n_l,
-  #         email: env['omniauth.auth']['info']['email']
-  #       )
-  #     else
-  #       user_id = user[:id]
-  #     end
-
-  #     #and then the oa_account linked to the user
-  #     DB[:oa_account].update(
-  #       id:    env['omniauth.auth']['uid'],
-  #       type:  env['omniauth.auth']['provider'], 
-  #       user:  user_id,
-  #       token: env['omniauth.auth']['credentials']['token'],
-  #       uri:   auth_uri_helper(env['omniauth.auth']),
-  #       img:   img,
-  #       email: env['omniauth.auth']['info']['email'],
-  #       n_f:   n_f, #names[0],
-  #       n_l:   n_l, #names[-1],
-  #       n_n:   env['omniauth.auth']['info']['nickname'],
-  #       n_m:   n_m,
-  #       location: env['omniauth.auth']['info']['location']
-  #     )
- 
-  #     #load the account
-  #     oa_account = DB[:oa_account].where(id: env['omniauth.auth']['uid']).where(type: env['omniauth.auth']['provider'])
-  #     session[:id] = user_id
-  #   else
-  #     # if we already had a record to update, check to see whether the user link exists so we can mark them 
-  #     # as a logged in AND registered user
-  #     p "setting session id: #{oa_account[:user][:user]}"
-  #     session[:id] = oa_account[:user][:user] unless oa_account.nil? || oa_account[:user].nil?
-  #   end
-
-  #   #push through to the session
-  #   oa = oa_account.first
-  #   p "oa = #{oa}"
-  #   p "oa[:type] = #{oa[:type]}"
-  #   p "oa[:type].to_sym = #{oa[:type].to_sym}"
-  #   session[:oa] ||= {}
-  #   session[:oa][oa[:type].to_sym] = oa[:id]
-  # end
 
   # redirect to("/")
 end
 
-
-
-# projects and handles
-get '/:handle' do
-  # look up projects
-  @project = Project.find_by(short: params[:handle])
-  unless @project.id
-    @profile = User.find_by(slack_name: params[:handle])
-    unless @profile.id
-      halt 404
-    else
-      erb :profile, locals: {klass: :profile}
-    end
-  end
-  erb :project, locals: {klass: :project}
+get '/auth/failure' do
+   erb "<h1>Authentication Failed:</h1><h3>message:<h3> <pre>#{params}</pre>"
 end
+
+get '/logout' do
+  session[:id] = nil
+  redirect to("/")
+end
+
+get '/auth/slack/deauthorized' do
+  erb "Slack has deauthorized this app."
+end
+
+# # projects and handles
+# get '/:handle' do
+#   # look up projects
+#   @project = Project.find_by(short: params[:handle])
+#   unless @project && @project.id
+#     @profile = User.find_by(slack_name: params[:handle])
+#     unless @profile.id
+#       halt 404
+#     else
+#       erb :profile, locals: {klass: :profile}
+#     end
+#   end
+#   erb :project, locals: {klass: :project}
+# end
 
 
 
