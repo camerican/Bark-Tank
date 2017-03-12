952502c35ced5d72b64fbcf47a0087570f754037
Cam Crews <camcrews@gmail.com>
2017-03-12 15:57:14 -0400

    configuration change

--- server.rb
+++ server.rb
@@ -17,7 +17,23 @@ include ActionView::Context
 
 configure do
   enable :sessions
+end
+
+configure :development, :test do
   set :database, {adapter: 'sqlite3', database: 'db/barktank.sqlite3'}
+end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
 end
 
 use OmniAuth::Builder do
